cf88e351fff9aeba6920915104f6534593a6ddee
poffioleg <oleg.l.log@gmail.com>
2024-03-17 19:35:05 +0300

    zxcv

--- register.php
+++ register.php
@@ -5,7 +5,7 @@ echo $tel = $_POST['tel'];
 echo $email = $_POST['email'];
 echo $date = $_POST['date'];
 echo $someGroupName = $_POST['someGroupName'];
-echo $language = $_POST['language'];
+echo $languages = $_POST['languages'];
 echo $bio = $_POST['bio'];
 echo $checkt = $_POST['checkt'];
 ?>
