f3d04ab1a86f3491ffd1671ee391381a7b645a61
poffioleg <oleg.l.log@gmail.com>
2024-03-17 21:37:53 +0300

    asfdasdasd

--- db.php
+++ db.php
@@ -24,7 +24,7 @@ $db = new PDO(
 //Подготовленный запрос. Не именованные метки.
 // try {
 //$stmt = $db->prepare("INSERT INTO forms SET name,email = ?");
-$stmt = $db->prepare("INSERT INTO forms (name, email, massage) VALUES ('$login','$tel','$email'");
+$stmt = $db->prepare("INSERT INTO forms (name, email, massage) VALUES ('$name','$email','$massage'");
 // $stmt->execute([$_POST['login']], );
 // $stmt->execute([$_POST['tel']], );
 // $stmt->execute([$_POST['email']], );
