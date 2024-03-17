93e31482634b41a698b6bb9147616e7263786a78
poffioleg <oleg.l.log@gmail.com>
2024-03-17 20:18:46 +0300

    qwertwqret

--- db.php
+++ db.php
@@ -22,17 +22,12 @@ $db = new PDO(
 ); // Заменить test на имя БД, совпадает с логином uXXXXX
 
 //Подготовленный запрос. Не именованные метки.
-// try {
-//     $stmt = $db->prepare("INSERT INTO forms SET name = ?");
-//     $stmt->execute([$_POST['login'], $_POST['email']]);
-// } catch (PDOException $e) {
-//     print ('Error : ' . $e->getMessage());
-//     exit();
-// }
-$sql = "INSERT INTO forms (login, tel, email) VALUES ('$login', '$tel', '$email')";
-if ($conn->query($sql) === TRUE) {
-    $form_id = $conn->insert_id; // Получаем ID новой записи
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+try {
+    $stmt = $db->prepare("INSERT INTO forms SET (name,email,massege) = ?");
+    $stmt->execute([$_POST['login'], $_POST['email'], $_POST['tel']]);
+} catch (PDOException $e) {
+    print ('Error : ' . $e->getMessage());
+    exit();
 }
+
 ?>
