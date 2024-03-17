761502d092c8b17c945b4be3a21f07f1a77bca55
poffioleg <oleg.l.log@gmail.com>
2024-03-17 21:52:58 +0300

    ewrwer

--- db.php
+++ db.php
@@ -25,7 +25,7 @@ $db = new PDO(
 try {
     //$stmt = $db->prepare("INSERT INTO forms SET name,email = ?");
     $stmt = $db->prepare("INSERT INTO forms (name, email, massage) VALUES ('login','tel','email'");
-    $stmt->execute([$_POST['login'], $_POST['tel'], $_POST['email']]);
+
 
 
 
