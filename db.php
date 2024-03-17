50a41401f58733153bfd63a44194fd03baee462c
poffioleg <oleg.l.log@gmail.com>
2024-03-17 21:26:58 +0300

    2werfwf

--- db.php
+++ db.php
@@ -22,16 +22,20 @@ $db = new PDO(
 ); // Заменить test на имя БД, совпадает с логином uXXXXX
 
 //Подготовленный запрос. Не именованные метки.
-try {
-    //$stmt = $db->prepare("INSERT INTO forms SET name,email = ?");
-    $stmt = $db->prepare("INSERT INTO forms (name, email, massage) VALUES (:name, :email, :massage)");
-    $stmt->execute([$_POST['login']], );
-    $stmt->execute([$_POST['tel']], );
-    $stmt->execute([$_POST['email']], );
-
-} catch (PDOException $e) {
-    print ('Error : ' . $e->getMessage());
-    exit();
+// try {
+//$stmt = $db->prepare("INSERT INTO forms SET name,email = ?");
+$stmt = $db->prepare("INSERT INTO forms (name, email, massage) VALUES ('$login','$tel','$email'");
+// $stmt->execute([$_POST['login']], );
+// $stmt->execute([$_POST['tel']], );
+// $stmt->execute([$_POST['email']], );
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
+// } catch (PDOException $e) {
+//     print ('Error : ' . $e->getMessage());
+//     exit();
+// }
 
 ?>
