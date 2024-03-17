d74d37d27973f6c43c9ea2d2d5795bd23f6c3f02
poffioleg <oleg.l.log@gmail.com>
2024-03-17 20:04:13 +0300

    asdfancvb

--- register.php
+++ register.php
@@ -19,18 +19,18 @@ $checkt = $_POST['checkt'];
 // }
 
 // Сохранение выбранных ЯП в таблице "form_languages"
-foreach ($languages as $language) {
-    $language = $conn->real_escape_string($language);
-    $sql = "INSERT INTO languages (language_name) VALUES ('$language')";
-    if ($conn->query($sql) === TRUE) {
-        $language_id = $conn->insert_id; // Получаем ID новой записи
-        $sql = "INSERT INTO form_languages (form_id, language_id) VALUES ('$form_id', '$language_id')";
-        $conn->query($sql);
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-}
+// foreach ($languages as $language) {
+//     $language = $conn->real_escape_string($language);
+//     $sql = "INSERT INTO languages (language_name) VALUES ('$language')";
+//     if ($conn->query($sql) === TRUE) {
+//         $language_id = $conn->insert_id; // Получаем ID новой записи
+//         $sql = "INSERT INTO form_languages (form_id, language_id) VALUES ('$form_id', '$language_id')";
+//         $conn->query($sql);
+//     } else {
+//         echo "Error: " . $sql . "<br>" . $conn->error;
+//     }
+// }
 
-// Закрытие соединения с базой данных
-$conn->close();
+// // Закрытие соединения с базой данных
+// $conn->close();
 ?>
