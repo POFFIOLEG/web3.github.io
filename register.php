f9a73787ba67c666ac8b173ef9a97aed375087ef
poffioleg <oleg.l.log@gmail.com>
2024-03-17 19:09:49 +0300

    324234234234

--- register.php
+++ register.php
@@ -1,24 +1,28 @@
 <?php
 // Подключение к базе данных
-$servername = "localhost";
-$username = "u67307";
-$password = "2532509";
-$dbname = "forms";
+// $servername = "localhost";
+// $username = "u67307";
+// $password = "2532509";
+// $dbname = "forms";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+// $conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die ("Connection failed: " . $conn->connect_error);
-}
+// if ($conn->connect_error) {
+//     die ("Connection failed: " . $conn->connect_error);
+// }
 
+$user = 'u67307'; // Заменить на ваш логин uXXXXX
+$pass = '2532509'; // Заменить на пароль, такой же, как от SSH
+$db = new PDO('mysql:host=localhost;dbname=forms', $user, $pass,
+  [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); /
 // Обработка данных из формы
-$name = $_POST['name'];
+$login = $_POST['login'];
 $email = $_POST['email'];
 $tel = $_POST['tel'];
 $languages = $_POST['languages']; // ЯП передаются в виде массива
 
 // Сохранение данных из формы в таблице "forms"
-$sql = "INSERT INTO forms (name, email, message) VALUES ('$name', '$email', '$message')";
+$sql = "INSERT INTO forms (login, email, tel) VALUES ('$login', '$email', '$tel')";
 if ($conn->query($sql) === TRUE) {
     $form_id = $conn->insert_id; // Получаем ID новой записи
 } else {
@@ -28,7 +32,7 @@ if ($conn->query($sql) === TRUE) {
 // Сохранение выбранных ЯП в таблице "form_languages"
 foreach ($languages as $language) {
     $language = $conn->real_escape_string($language);
-    $sql = "INSERT INTO languages (language_name) VALUES ('$language')";
+    $sql = "INSERT INTO languages (language_login) VALUES ('$language')";
     if ($conn->query($sql) === TRUE) {
         $language_id = $conn->insert_id; // Получаем ID новой записи
         $sql = "INSERT INTO form_languages (form_id, language_id) VALUES ('$form_id', '$language_id')";
