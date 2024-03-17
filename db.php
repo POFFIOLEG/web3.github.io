d0a893232a0879d0e0f11f51c783857917ee2303
poffioleg <oleg.l.log@gmail.com>
2024-03-17 14:01:32 +0300

    123412341234

---
+++ db.php
@@ -0,0 +1,13 @@
+<?PHP
+
+$servername = "kubsu-dev.ru";
+$username = "u67307";
+$password = "2532509";
+$dbname = "users";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die ("Connection Fialed" . mysqli_connect_error());
+} else {
+    echo "Good";
+} ?>
--- register.php
+++ register.php
@@ -1,10 +1,10 @@
 <?php
-
-echo $_POST['login'];
-echo $_POST['tel'];
-echo $_POST['e-mail'];
-echo $_POST['date'];
-echo $_POST['someGroupName'];
-echo $_POST['vibor'];
-echo $_POST['bio'];
-echo $_POST['check'];
+require_once ('db.php');
+$login = $_POST['login'];
+$tel = $_POST['tel'];
+$email = $_POST['e-mail'];
+$date = $_POST['date'];
+$someGroupName = $_POST['someGroupName'];
+$vibor = $_POST['vibor'];
+$bio = $_POST['bio'];
+$check = $_POST['check'];
