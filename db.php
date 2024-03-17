625750391db85f548c319d6952b69a81d1dad28f
poffioleg <oleg.l.log@gmail.com>
2024-03-17 21:57:19 +0300

    234324

--- db.php
+++ db.php
@@ -24,21 +24,13 @@ $db = new PDO(
 //Подготовленный запрос. Не именованные метки.
 try {
     //$stmt = $db->prepare("INSERT INTO forms SET name,email = ?");
-    $stmt = $db->prepare("INSERT INTO forms (name, email, massage) VALUES ('login','tel','email'");
-
-
-
-
-
-    // if ($conn->query($sql) === TRUE) {
-//     echo "New record created successfully";
-// } else {
-//     echo "Error: " . $sql . "<br>" . $conn->error;
-// }
-
+    $stmt = $db->prepare("INSERT INTO forms (name, email, massage) VALUES ('login', 'tel', 'email')");
+    $stmt->execute([$_POST['login']], );
+    $stmt->execute([$_POST['tel']], );
 
 } catch (PDOException $e) {
     print ('Error : ' . $e->getMessage());
     exit();
 }
+
 ?>
