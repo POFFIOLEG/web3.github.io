aad051de188410ffd58b21bbb172d6c506265895
poffioleg <oleg.l.log@gmail.com>
2024-03-17 20:47:23 +0300

    afdsfasdrf

--- db.php
+++ db.php
@@ -25,8 +25,7 @@ $db = new PDO(
 try {
     $stmt = $db->prepare("INSERT INTO forms SET name = ?");
     $stmt->execute([$_POST['login']], );
-    $stmt = $db->prepare("INSERT INTO forms SET email = ?");
-    $stmt->execute([$_POST['email']]);
+
 } catch (PDOException $e) {
     print ('Error : ' . $e->getMessage());
     exit();
