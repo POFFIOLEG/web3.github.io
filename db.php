f8712b5d6dc685285dbcfaa68dfd0cfd35432120
poffioleg <oleg.l.log@gmail.com>
2024-03-17 19:39:07 +0300

    12we

--- db.php
+++ db.php
@@ -23,8 +23,8 @@ $db = new PDO(
 
 // Подготовленный запрос. Не именованные метки.
 try {
-    $stmt = $db->prepare("INSERT INTO application SET name = ?");
-    $stmt->execute([$_POST['fio']]);
+    $stmt = $db->prepare("INSERT INTO forms SET name = ?");
+    $stmt->execute([$_POST['name']]);
 } catch (PDOException $e) {
     print ('Error : ' . $e->getMessage());
     exit();
