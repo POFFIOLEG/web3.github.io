a58ea304242a4eeeafd971a629eec6679c5c1c3a
poffioleg <oleg.l.log@gmail.com>
2024-03-17 19:34:16 +0300

    qwfwefqwefqwef

--- register.php
+++ register.php
@@ -2,10 +2,10 @@
 // require_once ('db.php');
 echo $login = $_POST['login'];
 echo $tel = $_POST['tel'];
-echo $email = $_POST['e-mail'];
+echo $email = $_POST['email'];
 echo $date = $_POST['date'];
 echo $someGroupName = $_POST['someGroupName'];
-echo $vibor = $_POST['vibor'];
+echo $language = $_POST['language'];
 echo $bio = $_POST['bio'];
 echo $checkt = $_POST['checkt'];
 ?>
