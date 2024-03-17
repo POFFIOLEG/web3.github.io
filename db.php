d22991a5dc24743c7ba73ba043bc323ae5ac86d5
poffioleg <oleg.l.log@gmail.com>
2024-03-17 15:00:32 +0300

    123444444444

--- db.php
+++ db.php
@@ -1,14 +1,15 @@
-<?PHP
+<?php
+$host = 'localhost'; // Хост базы данных (в данном случае localhost через SSH-туннель)
+$port = 3306; // Порт базы данных (может отличаться в зависимости от настроек SSH-туннеля)
+$dbname = 'users'; // Имя вашей базы данных
+$username = 'u67307'; // Имя пользователя базы данных
+$password = '2532509'; // Пароль пользователя базы данных
 
-$servername = "kubu-dev.ru";
-$username = "u67307";
-$password = "2532509";
-$dbname = "users";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if (!$conn) {
-    die ("Connection Fialed" . mysqli_connect_error());
-} else {
-    echo "Good";
+try {
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Соединение с базой данных установлено успешно";
+} catch (PDOException $e) {
+    echo "Ошибка подключения к базе данных: " . $e->getMessage();
 }
-?>Ы
+?>
