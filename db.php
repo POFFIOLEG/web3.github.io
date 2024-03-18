064938d048de33edae84b1fbb378c01293af980d
poffioleg <oleg.l.log@gmail.com>
2024-03-18 19:55:05 +0300

    wefwerf

--- db.php
+++ db.php
@@ -20,14 +20,14 @@ $db = new PDO(
 //     exit();
 // }
 try {
-    $stmt = $db->prepare("INSERT INTO forms (name, email,massage) VALUES (:login, :email,:massage)");
+    $stmt = $db->prepare("INSERT INTO forms (name, email,message) VALUES (:login, :email,:message)");
 
     $login = $_POST['login'];
     $email = $_POST['email'];
     $tel = $_POST['tel'];
     $stmt->bindParam(':login', $login);
     $stmt->bindParam(':email', $email);
-    $stmt->bindParam(':massage', $tel);
+    $stmt->bindParam(':message', $tel);
     $stmt->execute();
 } catch (PDOException $e) {
     print ('Error : ' . $e->getMessage());
