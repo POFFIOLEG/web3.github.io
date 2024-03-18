048cb1b2b0e5fa0ffc5472e19a27bef88edffa4c
poffioleg <oleg.l.log@gmail.com>
2024-03-18 20:25:43 +0300

    12341234

--- db.php
+++ db.php
@@ -20,7 +20,7 @@ $db = new PDO(
 //     exit();
 // }
 try {
-    $stmt = $db->prepare("INSERT INTO forms (full_name, phone,email,birth_date,gender,bio,contract_agreed) VALUES (:full_name, :phone,:email,:birth_date,:gender,:bio,:contract_agreed)");
+    $stmt = $db->prepare("INSERT INTO users (full_name, phone,email,birth_date,gender,bio,contract_agreed) VALUES (:full_name, :phone,:email,:birth_date,:gender,:bio,:contract_agreed)");
 
     $login = $_POST['login'];
     $email = $_POST['email'];
