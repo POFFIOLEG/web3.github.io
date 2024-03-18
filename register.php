a3d5abb9910e8fad88cd485aeea7660fb290767d
poffioleg <oleg.l.log@gmail.com>
2024-03-18 19:24:53 +0300

    dfgbfdgb

--- register.php
+++ register.php
@@ -1,13 +1,13 @@
 <?php
 // require_once ('prob.php');
-$login = $_POST['name'];
-$tel = $_POST['tel'];
-$email = $_POST['email'];
-$date = $_POST['date'];
-$someGroupName = $_POST['someGroupName'];
-$languages = $_POST['languages'];
-$bio = $_POST['bio'];
-$checkt = $_POST['checkt'];
+// $login = $_POST['login'];
+// $tel = $_POST['tel'];
+// $email = $_POST['email'];
+// $date = $_POST['date'];
+// $someGroupName = $_POST['someGroupName'];
+// $languages = $_POST['languages'];
+// $bio = $_POST['bio'];
+// $checkt = $_POST['checkt'];
 
 
 // $sql = "INSERT INTO forms (login, tel, email) VALUES ('$login', '$tel', '$email')";
