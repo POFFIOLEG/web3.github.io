7d43cfd48c98271300d0bcf1ae780b7b6fb9e348
poffioleg <oleg.l.log@gmail.com>
2024-03-24 16:04:40 +0300

    asdasd

--- procces_form.php
+++ procces_form.php
@@ -37,49 +37,20 @@ $db = new PDO(
     [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
 );
 
-// $sql = "SELECT users.id AS user_id, programming_languages.id AS lang_id 
-//         FROM users 
-//         JOIN users ON users.id = users.user_id 
-//         JOIN programming_languages ON programming_languages.id = users.lang_id";
-// $stmt = $db->query($sql);
 
-$sql = "SELECT u.id AS user_id, p.id AS language_id 
+
+
+$sql = "INSERT INTO user_languages (user_id, lang_id) 
+        SELECT u.id AS user_id, p.id AS programming_languages 
         FROM users u
-        JOIN user_programming_languages upl ON u.id = upl.user_id 
+        JOIN users upl ON u.id = upl.user_id 
         JOIN programming_languages p ON p.id = upl.language_id";
-$stmt = $db->query($sql);
 
+$stmt = $db->prepare($sql);
+$stmt->execute();
 
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    $user_id = $row['user_id'];
-    $language_id = $row['language_id'];
+echo "Данные успешно записаны в таблицу user_languages.";
 
-    // Создать SQL-запрос для вставки данных в таблицу user_languages
-    $insertSql = "INSERT INTO user_languages (user_id, lang_id) VALUES (:user_id, :lang_id)";
-    $insertStmt = $db->prepare($insertSql);
-    $insertStmt->bindParam(':user_id', $user_id);
-    $insertStmt->bindParam(':lang_id', $language_id);
-    $insertStmt->execute();
-}
-
-// Получение значения из table1
-// $stmt1 = $db->query("SELECT user_id FROM users");
-// $value1 = $stmt1->fetchColumn();
-
-// // Получение значения из table2
-// $stmt2 = $db->query("SELECT lang_id FROM programming_languages");
-// $value2 = $stmt2->fetchColumn();
-
-// // Подготовка SQL запроса для вставки данных в table3
-// $sql = "INSERT INTO user_languages (user_id, lang_id) VALUES (:user_id, :lang_id)";
-// $stmt3 = $db->prepare($sql);
-
-// // Привязка параметров и выполнение запроса
-// $stmt3->bindParam(':user_id', $value1, PDO::PARAM_STR);
-// $stmt3->bindParam(':lang_id', $value2, PDO::PARAM_STR);
-// $stmt3->execute();
-
-// echo "Данные успешно добавлены в таблицу table3.";
 
 
 ?>
