0dcc05d2ddfa6f9d51595d29ebfbd6fdb4e8f158
poffioleg <oleg.l.log@gmail.com>
2024-03-24 16:26:47 +0300

    123412341234

--- procces_form.php
+++ procces_form.php
@@ -1,33 +1,6 @@
 <?php
 
-// // $pdo = new PDO('mysql:host=localhost;dbname=u67307', 'пользователь', 'пароль');
-// $user = 'u67307';
-// $pass = '2532509';
-// $db = new PDO(
-//     'mysql:host=localhost;dbname=u67307',
-//     $user,
-//     $pass,
-//     [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-// );
 
-// $user_id = $_POST['user_id'];
-// $lang_id = $_POST['lang_id'];
-
-
-// $sql = "INSERT INTO user_languages (user_id, lang_id) VALUES (:user_id, :lang_id)";
-
-
-
-// $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
-// $stmt->bindParam(':lang_id', $lang_id, PDO::PARAM_INT);
-// $stmt->execute();
-
-
-
-
-
-
-// Подключение к базе данных
 $user = 'u67307';
 $pass = '2532509';
 $db = new PDO(
@@ -37,25 +10,21 @@ $db = new PDO(
     [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
 );
 
-$sql = "INSERT INTO user_languages (user_id, language_id) 
-        SELECT id_name AS user_id, id_names AS language_id 
+
+
+
+$sql = "INSERT INTO user_languages (user_id, lang_id)
+        SELECT u.id_name AS user_id, p.id_names AS lang_id
         FROM users u
-        JOIN users upl ON id_name = upl.user_id 
-        JOIN programming_languages p ON id_names = upl.language_id";
+        JOIN programming_languages p ON 1=1";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Связь успешно установлена!";
+} else {
+    echo "Ошибка: " . $sql . "<br>" . $conn->error;
+}
 
 
-
-// $sql = "INSERT INTO user_languages (user_id, lang_id) 
-//         SELECT id_name AS users, id_names AS programming_languages 
-//         FROM users u
-//         JOIN users upl ON u.id = id_name 
-//         JOIN programming_languages p ON p.id = id_names";
-
-$stmt = $db->prepare($sql);
-$stmt->execute();
-
-echo "Данные успешно записаны в таблицу user_languages.";
-
-
+$conn->close();
 
 ?>
