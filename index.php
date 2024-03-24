18857c4bd1d2628db4651f5ff7b7d0e47a72bfd3
poffioleg <oleg.l.log@gmail.com>
2024-03-24 12:12:45 +0300

    qwedqwed

--- index.php
+++ index.php
@@ -104,15 +104,7 @@
 
 
                 <div class="col-auto">
-                    <label for="programming_languages">Select programming languages:</label>
-                    <select name="programming_languages[]" multiple>
-                        <option value="1">JavaScript</option>
-                        <option value="2">Python</option>
-                        <option value="3">Java</option>
-                        <option value="4">C++</option>
-                    </select>
 
-                    <input type="submit" value="Submit">
                     <!-- <label for="validationCustom04" class="form-label">Любимый язык программирования</label>
                     <select class="form-select rounded-pill" id="validationCustom04" required="" multiple="multiple"
                         name="izuk">
@@ -169,6 +161,16 @@
             </form>
         </div>
     </div>
+
+    <label for="programming_languages">Select programming languages:</label>
+    <select name="programming_languages[]" multiple>
+        <option value="1">JavaScript</option>
+        <option value="2">Python</option>
+        <option value="3">Java</option>
+        <option value="4">C++</option>
+    </select>
+
+    <input type="submit" value="Submit">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php
-require_once ('db234.php');
+require_once ('db.php');
 $login = $_POST['login'];
 $tel = $_POST['tel'];
 $email = $_POST['email'];
@@ -8,4 +8,5 @@ $someGroupName = $_POST['someGroupName'];
 $izuk = $_POST['izuk'];
 $bio = $_POST['bio'];
 $checkt = $_POST['checkt'];
-?>
+$programming_languages[] = $_POST['programming_languages']
+    ?>
