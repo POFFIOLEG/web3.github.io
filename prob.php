fa2cfbeabc0c714e822da320e9cc5cd6277b8446
poffioleg <oleg.l.log@gmail.com>
2024-03-24 17:58:34 +0300

    asdfasdf

--- prob.php
+++ prob.php
@@ -16,12 +16,6 @@ try {
     $kl = implode($Languages);
     $stmt->bindParam(':lang_name', $kl);
     $stmt->execute();
-    $sql = "INSERT INTO user_languages (user_id, lang_id) 
-        SELECT u.id_name AS user_id, p.id_names AS lang_id 
-        FROM users u
-        JOIN user_languages upl ON u.id_name = upl.user_id
-        JOIN programming_languages p ON p.id_names = upl.lang_id";
-    $stmt = $db->prepare($sql);
 
 } catch (PDOException $e) {
     print ('Error : ' . $e->getMessage());
