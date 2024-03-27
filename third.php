8cd14ae11c1974db6577f9d8b648270cec3b94c1
poffioleg <oleg.l.log@gmail.com>
2024-03-27 21:33:00 +0300

    asdasd

--- third.php
+++ third.php
@@ -11,7 +11,7 @@ $db = new PDO(
 $user_id = $conn->insert_id; // Получаем ID последней добавленной записи в таблицу users
 try {
     $stmt = $db->prepare("INSERT INTO user_languages (user_id, language) VALUES (:user_id,:language)");
-    $user_id = $conn->insert_id;
+    $user_id = $conn->insert_user_id;
     $lange = $_POST['lange'];
 
     $kl = implode($Languages);
