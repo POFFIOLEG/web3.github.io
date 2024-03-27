81247654c505e5fe54874a0bb7b10d792df0d640
poffioleg <oleg.l.log@gmail.com>
2024-03-27 21:23:28 +0300

    qwedwqed

--- third.php
+++ third.php
@@ -1,32 +1,16 @@
 <?php
-// $user = 'u67307';
-// $pass = '2532509';
-// $db = new PDO(
-//     'mysql:host=localhost;dbname=u67307',
-//     $user,
-//     $pass,
-//     [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-// );
+$user = 'u67307';
+$pass = '2532509';
+$db = new PDO(
+    'mysql:host=localhost;dbname=u67307',
+    $user,
+    $pass,
+    [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+);
 
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
+$user_id = $conn->insert_id; // Получаем ID последней добавленной записи в таблицу users
 
+$sql_user_languages = "INSERT INTO user_languages (user_id, language) VALUES ('$user_id', 'Java'), ('$user_id', 'Python')";
 
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//     $user_id = $_POST['user_id'];
-//     $language_id = $_POST['language_id'];
-
-
-//     $sql = "INSERT INTO user_languages (user_id, language_id) VALUES ('$user_id', '$language_id')";
-
-//     if ($conn->query($sql) === TRUE) {
-//         echo "Данные успешно добавлены в таблицу user_languages.";
-//     } else {
-//         echo "Ошибка: " . $sql . "<br>" . $conn->error;
-//     }
-// }
-
-
-// $conn->close();
+$conn->close();
 ?>
