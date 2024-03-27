f7a87241370309271d451621191131b260d01935
poffioleg <oleg.l.log@gmail.com>
2024-03-27 18:26:48 +0300

    200win!

--- db.php
+++ db.php
@@ -26,25 +26,16 @@ try {
     $stmt->bindParam(':gender', $someGroupName);
     $stmt->bindParam(':bio', $bio);
     $stmt->bindParam(':contract_agreed', $checkt);
-    // $stmt = $db->prepare("INSERT INTO programming_languages (lang_name) VALUES (:lang_name)");
-    // $izuk = $_POST['izuk'];
-    // $stmt->bindParam(':lang_name', $izuk); Делать дальше через foreach
     $stmt->execute();
 } catch (PDOException $e) {
     print ('Error : ' . $e->getMessage());
     exit();
 }
+
 $Languages = $_POST['lange'];
 foreach ($Languages as $lange) {
     $stmt = $db->prepare("INSERT INTO programming_languages (lang_id, lang_name) VALUES (:lang_id, :lang_name)");
-    // $lange[] = $_POST['lange'];
-    // $kl = implode($Languages);
     $stmt->bindParam(':lang_name', $kl);
-    // $stmt->execute(['user_id' => $userId, 'lang_id' => $langId]);
 }
-
-
-
-
 
 ?>
